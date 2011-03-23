642cd1f9ddbace8a361b8a7a801fa5929cf3d8c0
unknown <Justin@.(none)>
2011-03-23 12:39:15 -0700

    updated files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110303185848) do
+ActiveRecord::Schema.define(:version => 20110309235436) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110303185848) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "administrator"
   end
 
 end
