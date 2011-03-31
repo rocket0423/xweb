dc111ccef0cce20c01d57af1a9c00f64c99a28e3
Justin Carstens <jrcarstens@cox.net>
2011-03-31 12:40:59 -0700

    removed

--- test/functional/updatetables_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UpdatetablesControllerTest < ActionController::TestCase
-  test "should get update" do
-    get :update
-    assert_redirected_to player_path
-  end
-
-end
